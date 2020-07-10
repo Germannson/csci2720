efbf189d610fd371795a0589d8e35820eeebc518
Germannson <halflifelai@gmail.com>
2020-07-10 21:57:53 +0800

    bootstrap-4-react

---
+++ react/src/Event.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'bootstrap-4-react';
+import './App.css';
+
+function Event() {
+  return (
+    <div className="container bg-info">
+      <div className="row">
+        <div className="col">Cool</div>
+        <div className="col">Warm</div>
+        <div className="col">Cold</div>
+      </div>
+      <div className="row">
+        <div className="col">Cool</div>
+        <div className="col">Warm</div>
+        <div className="col">Cold</div>
+      </div>
+      <div className="row">
+        <div className="col">Cool</div>
+        <div className="col">Warm</div>
+        <div className="col">Cold</div>
+      </div>
+    </div>
+    
+  );
+}
+
+
+export default Event;
---
+++ react/src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'bootstrap-4-react';
+
+function Header() {
+  return (
+    <header className="bg-warning">
+    <span className="display-4 text-left p-2">Event system</span>
+    <form>
+      <input className="align-right"></input>
+    </form>
+    </header>
+  );
+}
+
+
+export default Header;
