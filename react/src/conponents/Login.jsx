ced1fcc9e4a36c415e7d889699dce26498f16246
tcmak217 <tcmak217@outlook.com>
2020-07-12 21:30:15 +0800

    fix login form position

--- react/src/conponents/Login.jsx
+++ react/src/conponents/Login.jsx
@@ -1,42 +1,34 @@
 import React, { Component } from "react";
-import { Form, Button } from "bootstrap-4-react";
+import { Form, Button, Container } from "bootstrap-4-react";
 
 class Login extends Component {
   state = {};
   render() {
     return (
-      <Form>
-        <Form.Group>
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <Form.Input
-            type="email"
-            id="exampleInputEmail1"
-            placeholder="Enter email"
-          />
-          <Form.Text text="muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group>
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <Form.Input
-            type="password"
-            id="exampleInputPassword1"
-            placeholder="Password"
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Check>
-            <Form.CheckInput type="checkbox" id="exampleCheck1" />
-            <Form.CheckLabel htmlFor="exampleCheck1">
-              Check me out
-            </Form.CheckLabel>
-          </Form.Check>
-        </Form.Group>
-        <Button primary type="submit">
-          Submit
-        </Button>
-      </Form>
+      <Container>
+        <Form float="right">
+          <Form.Group>
+            <label htmlFor="exampleInputEmail1">User Name</label>
+            <Form.Input
+              type="email"
+              id="exampleInputEmail1"
+              placeholder="User Name"
+            />
+          </Form.Group>
+          <Form.Group>
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <Form.Input
+              type="password"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </Form.Group>
+
+          <Button primary type="submit">
+            Sign in
+          </Button>
+        </Form>
+      </Container>
     );
   }
 }
