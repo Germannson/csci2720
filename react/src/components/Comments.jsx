141d42d6d9eefc38556b0b55466e60bc7c47f84e
tcmak217 <tcmak217@outlook.com>
2020-07-15 00:23:47 +0800

    add EventDetail, AddComment, Comments components

--- react/src/components/Comments.jsx
+++ react/src/components/Comments.jsx
@@ -1,28 +1,82 @@
-import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
 
 function Event() {
   return (
-    <div className="container bg-info">
-      <div className="row">
-        <div className="col">Cool</div>
-        <div className="col">Warm</div>
-        <div className="col">Cold</div>
-      </div>
-      <div className="row">
-        <div className="col">Cool</div>
-        <div className="col">Warm</div>
-        <div className="col">Cold</div>
-      </div>
-      <div className="row">
-        <div className="col">Cool</div>
-        <div className="col">Warm</div>
-        <div className="col">Cold</div>
-      </div>
-    </div>
-    
+    <Container>
+      <Row className="mt-5">
+        <Col lg={1}>
+          <Image src="https://picsum.photos/50" roundedCircle />
+        </Col>
+        <Col lg={9}>
+          <div>
+            <span>User Name</span>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Assumenda aspernatur iste quidem odio ab, beatae voluptatibus
+              autem voluptatem voluptate dignissimos possimus fugit sit, natus
+              tempore culpa nam inventore architecto eum?
+            </p>
+          </div>
+        </Col>
+        <Col lg={2}></Col>
+      </Row>
+      <Row className="mt-5">
+        <Col lg={1}>
+          <Image src="https://picsum.photos/50" roundedCircle />
+        </Col>
+        <Col lg={9}>
+          <div>
+            <span>User Name</span>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Assumenda aspernatur iste quidem odio ab, beatae voluptatibus
+              autem voluptatem voluptate dignissimos possimus fugit sit, natus
+              tempore culpa nam inventore architecto eum?
+            </p>
+          </div>
+        </Col>
+        <Col lg={2}></Col>
+      </Row>
+      <Row className="mt-5">
+        <Col lg={1}>
+          <Image src="https://picsum.photos/50" roundedCircle />
+        </Col>
+        <Col lg={9}>
+          <div>
+            <span>User Name</span>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Assumenda aspernatur iste quidem odio ab, beatae voluptatibus
+              autem voluptatem voluptate dignissimos possimus fugit sit, natus
+              tempore culpa nam inventore architecto eum?
+            </p>
+          </div>
+        </Col>
+        <Col lg={2}></Col>
+      </Row>
+      <Row className="mt-5">
+        <Col lg={1}>
+          <Image src="https://picsum.photos/50" roundedCircle />
+        </Col>
+        <Col lg={9}>
+          <div>
+            <span>User Name</span>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Assumenda aspernatur iste quidem odio ab, beatae voluptatibus
+              autem voluptatem voluptate dignissimos possimus fugit sit, natus
+              tempore culpa nam inventore architecto eum?
+            </p>
+          </div>
+        </Col>
+        <Col lg={2}></Col>
+      </Row>
+    </Container>
   );
 }
-
 
 export default Event;
